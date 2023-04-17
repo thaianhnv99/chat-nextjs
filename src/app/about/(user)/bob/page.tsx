7128458ab9e7e6757ad1ad8efd9fbc38fae6184a
thaianhnv99 <thaianhnguyen283@gmail.com>
2023-04-17 17:53:26 +0700

    first commit

---
+++ src/app/about/(user)/bob/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <>Bob</>;
+};
+
+export default Page;
---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+interface PageProps {
+  children: React.ReactNode;
+}
+
+const Page = ({ children }: PageProps) => {
+  return <div>about</div>;
+};
+
+export default Page;
